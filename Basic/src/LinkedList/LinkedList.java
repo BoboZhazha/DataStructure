0b199d10531c09a5af2b8001d229718eebfff0df
zhang@mac <909319019@qq.com>
2019-03-05 18:16:43 +0800

    实现链表,调试打印

--- Basic/src/LinkedList/LinkedList.java
+++ Basic/src/LinkedList/LinkedList.java
@@ -5,9 +5,11 @@ public class LinkedList<E> {
     // 在链表里有一个Node的内部类
     private class Node {
         public E e;
+
+        // 下一个节点
         public Node next;
 
-        public Node(E e, Node next){
+        public Node(E e, Node next) {
             this.e = e;
             this.next = next;
         }
@@ -26,35 +28,153 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyHead;
     int size;
 
-    public LinkedList(){
-        head = null;
+    public LinkedList() {
+        dummyHead = new Node();
         size = 0;
     }
 
     // 获取元素的个数
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
     // 返回链表是否为空
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
+    }
+
+
+    public void add(int index, E e) {
+        // 找到要添加节点的前一个节点
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        // Node node = new Node(e);
+        // // 让新节点的next指向之前的那个被填加前的节点都下一个
+        // node.next = prev.next;
+        // // 让前一个指向新加的那个
+        // prev.next = node;
+
+        // 等效上面3句话
+        prev.next = new Node(e, prev.next);
+        size++;
+
     }
 
     // 头部添加
     public void addFirst(E e) {
-//        Node node = new Node(e);
-        // 让新节点的next指向之前的那个头(连接)
-//        node.next = head;
-        // 更新头部节点为新的节点
-//        head = node;
-
-        head = new Node(e, head);
-        size ++;
+        add(0, e);
     }
 
+    // 尾部添加
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    // 获得链表的第一个元素
+    public E getFirst() {
+        return get(0);
+    }
+
+    // 获得链表的最后一个元素
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    // 查找链表中是否有元素e
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+        }
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size --;
+        return retNode.e;
+    }
+
+    // 从链表中删除第一个元素, 返回删除的元素
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    // 从链表中删除最后一个元素, 返回删除的元素
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    // 从链表中删除元素e
+    public void removeElement(E e){
+
+        Node prev = dummyHead;
+        while(prev.next != null){
+            if (prev.next.e.equals(e)) {
+                break;
+            }
+            prev = prev.next;
+        }
+
+        if(prev.next != null){
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+            size --;
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+
+        Node cur = dummyHead.next;
+        while(cur != null){
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+
+        return res.toString();
+    }
 
 }
