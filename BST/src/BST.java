33d33579460f0e665d064b23f67d701ba6cf2ebc
zhang@mac <909319019@qq.com>
2019-03-24 14:07:26 +0800

    add BST module

---
+++ BST/src/BST.java
@@ -0,0 +1,67 @@
+
+// 二分搜索树要求存储的元素具有可比性
+public class BST<E extends Comparable<E>> {
+
+    // 内部类, 对用户来说是屏蔽的
+    private class Node {
+
+        //存放元素
+        public E e;
+
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    // 向二分搜索树中添加新的元素
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    // 向以node为根的二分搜索树中插入元素E, 递归算法
+    private void add(Node node, E e) {
+        if (e.equals(node.e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+        if (e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        } else {
+            add(node.right, e);
+        }
+    }
+}
