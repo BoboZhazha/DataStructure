5d47b156967773c0a6c65dcf910ea2d8462e2263
zhang@mac <909319019@qq.com>
2019-10-02 22:02:06 +0800

    add methods that search element to BST

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -61,4 +61,23 @@ public class BST<E extends Comparable<E>> {
     }
 
 
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    public boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+        if (e.compareTo(node.e) == 0) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+
+    }
+
+
 }
